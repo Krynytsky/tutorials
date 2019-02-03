6f8451d9b31e961e3fa17978871942e82d5606fa
Yurii Krynytsky <yuri.krynytsky@gmail.com>
2019-02-03 10:00:13 +0200

    add some html to index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo "hello my server"; ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Title of the site situated here</title>
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+        <button type="button" name="home"></button>
+      </div>
+    </header>
+      <?php echo "hello my server"; ?>
+
+  </body>
+</html>
