fed419c3c4207a7fed2aefef4189713be539c41f
Yurii Krynytsky <yuri.krynytsky@gmail.com>
2019-02-15 21:57:11 +0200

    restructured folders

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Krynytsky</title>
+    <style><?php include 'CSS/main.css'; ?></style>
+  </head>
+  <body>
+    <header>
+      <h1>Yurii Krynytsky</h1>
+      <p>Email: yurii.krynytsky@gmail.com</p>
+      <p>Phone:+380675949837</p>
+      <p>Address:Ukraine,Sambir 81400, Ivasyka str. </p>
+    </header>
+  <div class="job_offer">
+     <h3>Junior PHP developer</h3>
+  </div>
+
+
+  </body>
+</html>
