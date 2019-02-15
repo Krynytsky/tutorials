e41a007e2dee1838b7204ee16fada7d4a62d2621
Yurii Krynytsky <yuri.krynytsky@gmail.com>
2019-02-16 01:51:58 +0200

    CV prototipe

--- html/index.php
+++ html/index.php
@@ -8,14 +8,30 @@
   <body>
     <header>
       <h1>Yurii Krynytsky</h1>
-      <p>Email: yurii.krynytsky@gmail.com</p>
-      <p>Phone:+380675949837</p>
-      <p>Address:Ukraine,Sambir 81400, Ivasyka str. </p>
+      <p id="white_colour">Email: yurii.krynytsky@gmail.com</p>
+      <p id="white_colour">Phone:+380675949837</p>
+      <p id="white_colour">Address:Ukraine,Sambir 81400, Ivasyka str. </p>
     </header>
   <div class="job_offer">
-     <h3>Junior PHP developer</h3>
+     <h2>Junior PHP developer</h2>
   </div>
-
+  <div class="summary">
+    <h3>Summary:</h3><br>
+    <p id="black_colour">Junior programming developer with experience of programming in PHP. Also have a strong
+theoretical background in quality assurance. Experience in manual testing of web resources. Have a
+practice of writing test cases, checklists, bug reports. Understanding of SDLC.</p><br>
+  </div>
+  <div class="skils">
+     <h3>Skills:</h3>
+     <ul>
+        <li>Programming Languages /Technologies:C++,PHP,OOP, html/css;</li>
+        <li>IDE: Code::Blocks, DevC++,Atom;</li>
+        <li>Methodologies: Agile/Scrum;</li>
+        <li>Operating Systems: Windows , Linux;</li>
+        <li>Bug tracking system: Mantis;</li>
+        <li>VCS: GIT/GitHub</li>
+      </ul>
+  </div>
 
   </body>
 </html>
