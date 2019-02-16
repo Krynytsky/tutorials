3ba4104cc2df539915e9029cfef7bb927399e521
Yurii Krynytsky <yuri.krynytsky@gmail.com>
2019-02-17 00:26:44 +0200

    CV page done

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Krynytsky</title>
-    <style><?php include 'CSS/main.css'; ?></style>
+    <style><?php include 'CSS/style.css'; ?></style>
   </head>
   <body>
     <header>
@@ -14,26 +15,64 @@
     </header>
   <div class="job_offer">
       <h2>Junior PHP developer</h2>
-    </div>
+  </div>
   <div class="summary">
       <h3>Summary:</h3><br>
       <p id="black_colour">Junior programming developer with experience of programming in PHP. Also have a strong
 theoretical background in quality assurance. Experience in manual testing of web resources. Have a
 practice of writing test cases, checklists, bug reports. Understanding of SDLC.</p><br>
-</div>
-  <div class="skils">
-     <h3>Skills:</h3>
-     <ul>
-        <li>Programming Languages /Technologies:C++,PHP,OOP, html/css;</li>
-        <li>IDE: Code::Blocks, DevC++,Atom;</li>
-        <li>Methodologies: Agile/Scrum;</li>
-        <li>Operating Systems: Windows , Linux;</li>
-        <li>Bug tracking system: Mantis;</li>
-        <li>VCS: GIT/GitHub</li>
-      </ul>
   </div>
+<hr/>
+  <h3><b>Skills:</b></h3><hr/>
+  <article class="articles">
+          <ul>
+             <li>Programming Languages /Technologies:C++,PHP,OOP, html/css;</li>
+             <li>IDE: Code::Blocks, DevC++,Atom;</li>
+             <li>Methodologies: Agile/Scrum;</li>
+             <li>Operating Systems: Windows , Linux;</li>
+             <li>Bug tracking system: Mantis;</li>
+             <li>VCS: GIT/GitHub</li>
+           </ul>
+  </article>
 
-
-
+    <h3>Experience:</h3>
+    <hr/>
+    <b>February 2018</b><br>
+    <hr/>
+      <article class="articles">
+        <ul>
+          <li>QATestLab (http://training.qatestlab.com/)</li>
+          <li>Training project of testing the website prestashop.qatestlab.com.ua</li>
+          <li>Writing test cases, checklists.</li>
+          <li>Making such types of testing as: testing layout,functional testing, usability testing.</li>
+          <li>Adding bugs to the Mantis bug tracking system.</li>
+        </ul>
+      </article><hr>
+    <b>June 2015</b><br><hr/>
+      <article class="articles">
+        <ul>
+          <li>Defended thesis: "Modeling and forecasting change trends in the state ofpopulation’s material welfare."</li>
+          <li>Methods for predicting socio-economic indicators (Holt's method, Holt-Wintersseasonal method and exponential smoothing method) via MS Excel.</li>
+        </ul>
+      </article>
+      <h3>Education:</h3>
+      <hr/>
+        <article class="articles">
+          <ul>
+            <li>Lviv state academy of finance.</li>
+            <li>Master's degree in economic cybernetics.</li>
+          </ul>
+      </article>
+      <h3>Aditional information:</h3>
+      <hr/>
+        <article class="articles">
+          <ul>
+            <li>Languages: Ukrainian (native), English (intermidiate);</li>
+            <li>Marital status : single.</li>
+          </ul>
+        </article>
+    <footer>
+      <h2>Download in pdf</h2>
+    </footer>
   </body>
 </html>
